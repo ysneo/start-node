961c4d33ab400de194903de59910b54a52c6367e
ysneo <jinchn728@gmail.com>
2017-07-08 14:16:27 +0800

    do file system

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const http = require('http')
 const url = require('url');
+const fs = require('fs')
 
 // build a simple http server
 http.createServer((request, response) => {
@@ -8,7 +9,7 @@ http.createServer((request, response) => {
   response.end()
 }).listen(8000)
 
-// handling URL parameters
+// handling URL parameters http://127.0.0.1:8001/?name=Jack&age=24
 http.createServer((request, response) => {
   const params = url.parse(request.url, true).query
   response.writeHead(200, {
@@ -19,3 +20,26 @@ http.createServer((request, response) => {
     `.trim())
   response.end()
 }).listen(8001)
+
+// reading and writing file   http://127.0.0.1:8002/
+http.createServer((req, rsp) => {
+  if (req.url === '/') {
+    const fileName = './src/count.txt'
+    fs.readFile(fileName, 'utf-8', (er, data) => {
+      if (er) throw er
+      rsp.writeHead(200, { 'Content-Type': 'text/plain' })
+      rsp.write(`previous number is ${data}\n`)
+      const plus = parseInt(data) + 1
+      fs.writeFile(fileName, plus, () => {
+        rsp.write(`
+          Now the number is ${plus}
+         `.trim())
+        rsp.end('\nit\'s end.')
+      })
+    })
+  } else {
+    rsp.writeHead(404)
+    rsp.write('404 not found')
+    rsp.end()
+  }
+}).listen(8002)
