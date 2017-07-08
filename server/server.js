2ae6effdb8ea7ead88b2c30306d36467b0b90593
ysneo <jinchn728@gmail.com>
2017-07-08 13:31:02 +0800

    handling URL parameters

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const http = require('http')
+const url = require('url');
 
 // build a simple http server
 http.createServer((request, response) => {
@@ -6,3 +7,16 @@ http.createServer((request, response) => {
     response.write('You did it!')
     response.end()
 }).listen(8000)
+
+// handling URL parameters
+http.createServer((request, response) => {
+    const params = url.parse(request.url, true).query
+    response.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+    response.write(`
+        Here is your data from url: ${params.name} is ${params.age} years old.
+    `.trim())
+    response.end()
+}).listen(8001)
+
